566a5f3cc1831ba3a55d0dcd00be0afb501be4ee
naimanton <93698269+naimanton@users.noreply.github.com>
2022-03-06 22:12:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('well well...');
+function balance(grnz) {
+	const https = require('https');
+	let data = '', reg = /:value="(.+)"  r/;
+	https.get(
+		'https://spos.kz/t/zhol?account=' + grnz + '&check=1',
+		res => {
+			res.on('data', ch => data += ch);
+			res.on('end', () => console.log( data.match(reg)[1] ));
+		}
+	);
+}
+
+balance('363hia09');
